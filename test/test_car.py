9e27906d57aff89a91de67ae1dfd707b11f65a4f
Duc Quang Tran <ducquangtran@Ducs-Mac-mini.attlocal.net>
2023-03-11 00:46:46 -0800

    fix test cases

--- test/test_car.py
+++ test/test_car.py
@@ -1,17 +1,77 @@
 import unittest
 from datetime import datetime
-from ..car_factory import CarFactory
 
-class Testcalliope(unittest.TestCase):
-    def Test_battery_should_be_serviced(self):
+from ..engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+from battery.nubbin_battery import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
+
+#Test Batteries
+class TestSpindlerBattery(unittest.TestCase):
+    def test_needs_service_true(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year = today.year-3)
-        current_mileage = 0
+        last_service_date = today.replace(year=today.year - 3)
+        battery = SpindlerBattery(today, last_service_date)
+        self.assertTrue(battery.needs_service())
+
+    def test_needs_service_false(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 1)
+        battery = SpindlerBattery(today, last_service_date)
+        self.assertFalse(battery.needs_service())
+
+class TestNubbinBattery(unittest.TestCase):
+    def test_needs_service_true(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 5)
+        battery = NubbinBattery(today, last_service_date)
+        self.assertTrue(battery.needs_service())
+
+    def test_needs_service_false(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 1)
+        battery = NubbinBattery(today, last_service_date)
+        self.assertFalse(battery.needs_service())
+
+#Test Engines
+class TestWilloughbyEngine(unittest.TestCase):
+    def test_needs_service_true(self):
+        current_mileage = 92384
         last_service_mileage = 0
-        
-        car = CarFactory.create_calliope(today, last_service_date, current_mileage, last_service_mileage)
-        self.assertTrue(car.needs_service())
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        self.assertTrue(engine.needs_service())
 
-if __name__ == "__main__":
+    def test_needs_service_false(self):
+        current_mileage = 4000
+        last_service_mileage = 0
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        self.assertFalse(engine.needs_service())
+
+
+class TestSternmanEngine(unittest.TestCase):
+    def test_needs_service_true(self):
+        warning_light_is_on = True
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertTrue(engine.needs_service())
+
+    def test_needs_service_false(self):
+        warning_light_is_on = False
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertFalse(engine.needs_service())
+
+class TestCapuletEngine(unittest.TestCase):
+    def test_needs_service_true(self):
+        current_mileage = 45223
+        last_service_mileage = 0
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        self.assertTrue(engine.needs_service())
+
+    def test_needs_service_false(self):
+        current_mileage = 10000
+        last_service_mileage = 0
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        self.assertFalse(engine.needs_service())
+
+if __name__ == '__main__':
     unittest.main()
-
